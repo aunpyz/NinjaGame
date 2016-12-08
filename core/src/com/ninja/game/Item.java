cf7f4ed0f4d87c7a501176a2f23f120babf5dc8a
Aunpyz <aunnop_kattiyanet@hotmail.com>
2016-12-08 15:57:38 +0700

    interfaces

---
+++ core/src/com/ninja/game/Item.java
@@ -0,0 +1,11 @@
+package com.ninja.game;
+
+/**
+ * Created by Aunpyz on 12/8/2016.
+ */
+public interface Item{
+    String getName();
+    double getAttack();
+    double getHP();
+    boolean isDamagable();
+}
