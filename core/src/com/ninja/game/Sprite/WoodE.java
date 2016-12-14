5d2428a1a048ad3de99d0b05f5099f0bf8824375
Aunpyz <aunnop_kattiyanet@hotmail.com>
2016-12-15 06:44:46 +0700

    all enemy without Ai

--- core/src/com/ninja/game/Sprite/WoodE.java
+++ core/src/com/ninja/game/Sprite/WoodE.java
@@ -1,21 +1,17 @@
-package com.ninja.game.Scene;
+package com.ninja.game.Sprite;
 
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.ninja.game.Sprite.Character;
-import com.ninja.game.Sprite.SEnemy;
 
 /**
  * Created by Aunpyz on 12/15/2016.
  */
-public class WaterE extends SEnemy {
-    public WaterE(Skin skin) {
+public class WoodE extends SEnemy{
+    public WoodE(Skin skin) {
         super(skin);
     }
-
     @Override
     public void init()
     {
@@ -28,28 +24,28 @@ public class WaterE extends SEnemy {
         {
             for(int j=0; j<30; j++)
             {
-                idle[i][j] = resource.getRegion((i==0?"wfi":"wbi")+String.format("%02d", j+1));
+                idle[i][j] = resource.getRegion((i==0?"gfi":"gbi")+String.format("%02d", j+1));
             }
         }
         for(int i=0; i<2; i++)
         {
             for(int j=0; j<30; j++)
             {
-                die[i][j] = resource.getRegion((i==0?"wfd":"wbd")+String.format("%02d", j+1));
+                die[i][j] = resource.getRegion((i==0?"gfd":"gbd")+String.format("%02d", j+1));
             }
         }
         for(int i=0; i<2; i++)
         {
             for(int j=0; j<30; j++)
             {
-                attack[i][j] = resource.getRegion((i==0?"wfa":"wba")+String.format("%02d", j+1));
+                attack[i][j] = resource.getRegion((i==0?"gfa":"gba")+String.format("%02d", j+1));
             }
         }
         for(int i=0; i<2; i++)
         {
             for(int j=0; j<30; j++)
             {
-                walk[i][j] = resource.getRegion((i==0?"wfw":"wbw")+String.format("%02d", j+1));
+                walk[i][j] = resource.getRegion((i==0?"gfw":"gbw")+String.format("%02d", j+1));
             }
         }
         animation = new Animation(fps, idle[0]);
