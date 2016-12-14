c36f45212ea8511aa252cb877de0eed6cd65e07c
Aunpyz <aunnop_kattiyanet@hotmail.com>
2016-12-15 05:40:49 +0700

    add files

--- core/src/com/ninja/game/Scene/WaterE.java
+++ core/src/com/ninja/game/Scene/WaterE.java
@@ -1,5 +1,6 @@
 package com.ninja.game.Scene;
 
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.ninja.game.Sprite.SEnemy;
 
@@ -9,5 +10,22 @@ import com.ninja.game.Sprite.SEnemy;
 public class WaterE extends SEnemy {
     public WaterE(Skin skin) {
         super(skin);
+    }
+
+    @Override
+    public void init()
+    {
+        idle = new TextureAtlas[][];
+        die = new TextureAtlas[][];
+        attack = new TextureAtlas[][];
+        walk = new TextureAtlas[][];
+
+        for(int i=0; i<2; i++)
+        {
+            for(int j=0; j<30; j++)
+            {
+                idle[i][j] = resource.getRegion((i==0?"pfi":"pbi")+String.format("%02d", j+1));
+            }
+        }
     }
 }
