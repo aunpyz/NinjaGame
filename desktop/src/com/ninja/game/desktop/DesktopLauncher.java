40b7c599c990317e346228485ef5a751c2ca9229
Aunpyz <aunnop_kattiyanet@hotmail.com>
2016-12-15 03:07:17 +0700

    scale all img

--- desktop/src/com/ninja/game/desktop/DesktopLauncher.java
+++ desktop/src/com/ninja/game/desktop/DesktopLauncher.java
@@ -2,6 +2,8 @@ package com.ninja.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
 import com.ninja.game.GameMain;
 
 public class DesktopLauncher {
